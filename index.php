c45626b0081fc4c65dd6e7dd8e22993a36f1f935
e-miltenburg <User@User-PC>
2014-09-05 21:44:47 +0200

    test

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <title>Portfolio alpha</title>
+    </head>
+    <body>
+                <div id="big_wrapper">
+            <!--<header id="top_header"></*?php include 'Head.php' */?></header>-->
+                    <header id="top_header" ><img src="Images/general/headerBG.jpg"</header>
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                </div>
+        <article>
+                    <div class="projects"><p>project 1</p></div>
+                    </article>
+    </body>
+</html>
